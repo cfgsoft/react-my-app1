6b493616b7725cc62d4227edefd362e137abb319
it <it@chance-ltd.ru>
2018-07-11 18:17:48 +0400

    Button2

--- src/Hello.js
+++ src/Hello.js
@@ -1,19 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 class Hello extends React.Component {
 	constructor(props) {
     	super(props);
 
         this.state = {welcome: "Добро пожаловать на сайт!",
-                      label: "Нажми!"};
+                      label: "Нажми!",
+                  	  date: new Date()};
 
         this.press = this.press.bind(this);
     }
 
-    press(){
+	componentWillReceiveProps(nextProps) {
+		console.log("componentWillReceiveProps()");
+	}
+	componentWillMount(){
+	   console.log("componentWillMount()");
+	}
+	componentDidMount(){
+	   console.log("componentDidMount()");
+
+	   /*this.timerId = setInterval(()=> this.tick(), 5000);*/
+	}
+	componentWillUnmount(){
+	   console.log("componentWillUnmount()");
+	}
+	shouldComponentUpdate(){
+	   console.log("shouldComponentUpdate()");
+	   return true;
+	}
+	componentWillUpdate(){
+	   console.log("componentWillUpdate()");
+	}
+	componentDidUpdate(){
+	   console.log("componentDidUpdate()");
+	}
+
+    press(e){
     	let className = (this.state.class==="off")?"on":"off";
         this.setState({class: className});
-        this.setState({welcome: "Нажали"});
+        this.setState({welcome: "Нажали",
+        	           date: new Date()
+                      });        
+
+        /*console.log(e);*/
+    }
+
+    tick() {
+      this.setState({
+        date: new Date()
+      });
     }
 
     render() {  	
@@ -28,6 +64,7 @@ class Hello extends React.Component {
     		>
     		        {this.state.label}
     		 </button>
+    		 <h2>Текущее время {this.state.date.toLocaleTimeString()}.</h2>
      	</div>);
   	}
 }
